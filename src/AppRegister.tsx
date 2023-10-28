747f10847a63f5c3c6c921c11779ed1c44c9bd96
nimod7890 <nimod1234@g.skku.edu>
2023-10-28 16:08:32 +0900

    [TSK-1] setting reactQuery

--- src/AppRegister.tsx
+++ src/AppRegister.tsx
@@ -1,7 +1,13 @@
 import { ReactNode } from "react";
+import { BrowserRouter } from "react-router-dom";
+import ReactQuerySetting from "./app/libraries/reactQuery";
 
 type AppRegisterProps = { children: ReactNode };
 
 export default function AppRegister({ children }: AppRegisterProps) {
-  return <div>{children}</div>;
+  return (
+    <ReactQuerySetting>
+      <BrowserRouter>{children}</BrowserRouter>
+    </ReactQuerySetting>
+  );
 }
