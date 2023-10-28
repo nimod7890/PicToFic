b24f46984f8b9235238a6668e2a60d014f0f4a36
nimod7890 <nimod1234@g.skku.edu>
2023-10-28 20:16:17 +0900

    [TSK-9] create base page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,11 @@
 import AppLayout from "./AppLayout";
 import AppRegister from "./AppRegister";
-import Header from "./components/base/header/Header";
 import AppRouter from "./routes/AppRouter";
 
 export default function App() {
   return (
     <AppRegister>
       <AppLayout>
-        <Header/>
         <AppRouter/>
       </AppLayout>
     </AppRegister>
