0d7564281bac8091244b51df0f580787f0c2c324
nimod7890 <nimod1234@g.skku.edu>
2023-10-29 19:39:55 +0900

    [TSK-11] 토큰 유무에 따라 다른 라우터 보여주기

--- src/app/pages/AuthenticatedBasePage.tsx
+++ src/app/pages/AuthenticatedBasePage.tsx
@@ -4,7 +4,7 @@ import Header from "../components/base/header/Header";
 import { Layout } from "../constants/globalSizes";
 import MainContent from "../components/base/MainContent";
 
-export default function BasePage() {
+export default function AuthenticatedBasePage() {
   return (
     <Stack
       sx={{
--- src/app/routes/AppRouter.tsx
+++ src/app/routes/AppRouter.tsx
@@ -1,30 +1,9 @@
-import { Route, Routes } from "react-router-dom";
-import RoutePath from "./routePath";
-import BasePage from "../pages/BasePage";
-import HomePage from "../pages/HomePage";
-import UserPage from "../pages/UserPage";
 import useAuthStorage from "../hooks/useAuthStorage";
+import AuthenticatedRouter from "./AuthenticatedRouter";
+import UnauthenticatedRouter from "./UnauthenticatedRouter";
 
 export default function AppRouter() {
   const { isEmptyToken } = useAuthStorage();
 
-  return (
-    <Routes>
-      {isEmptyToken ? (
-        <>
-          {/* if token does not exists */}
-          <Route path={RoutePath.Login} element={"login page"} />
-          <Route path="*" element={<UserPage />} />
-        </>
-      ) : (
-        <>
-          {/* if token exists */}
-          <Route path={RoutePath.Index} element={<BasePage />}>
-            <Route path={RoutePath.Home} element={<HomePage />} />
-            <Route path="*" element={<UserPage />} />
-          </Route>
-        </>
-      )}
-    </Routes>
-  );
+  return isEmptyToken ? <UnauthenticatedRouter /> : <AuthenticatedRouter />;
 }
