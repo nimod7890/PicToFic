93bad542aa8cd191987c5cfdafdd92ef3ae58e98
nimod7890 <nimod1234@g.skku.edu>
2023-10-30 18:09:14 +0900

    [TSK-14] update mock files

--- src/app/api/post/getPostsByUserId.ts
+++ src/app/api/post/getPostsByUserId.ts
@@ -1,8 +1,8 @@
 import { SummaryPostListSchema } from "../../types/post";
-import { mockPosts } from "../../mocks/post";
+import { mockPosts, mockPosts3 } from "../../mocks/post";
 
 // export async function getPostsByUserId(): Promise<> {
 export function getPostsByUserId({ userId }: { userId: string }): SummaryPostListSchema {
-  const posts = userId === "nim_od" ? mockPosts : mockPosts;
+  const posts = userId === "nim_od" ? [...mockPosts3, ...mockPosts] : mockPosts;
   return posts;
 }
