983c89e04a51c0a9ef4843ce20f43cbbd2dcfdfa
nimod7890 <nimod1234@g.skku.edu>
2023-10-30 21:52:45 +0900

    [TSK-14] modify mock data

--- src/app/mocks/post.ts
+++ src/app/mocks/post.ts
@@ -6,7 +6,7 @@ export const mockPosts: SummaryPostListSchema = Array.from({ length: 5 }).map(()
   summary: "Dolore est ad nostrud id dolor. ",
   likesCount: 321,
   isLiked: true,
-  picturedAt: moment(new Date()).format("LLLL"),
+  picturedAt: moment(new Date()).format("LLL"),
 }));
 
 export const mockPosts2: SummaryPostListSchema = Array.from({ length: 3 }).map(() => ({
@@ -15,7 +15,7 @@ export const mockPosts2: SummaryPostListSchema = Array.from({ length: 3 }).map((
     "Consequat cillum quis reprehenderit non laboris sunt proident nulla cillum. Esse est veniam do commodo velit dolor esse. Ipsum duis fugiat ullamco do. Officia nulla veniam culpa minim eu laborum cillum minim sint irure. Sit irure est ea est nulla labore mollit dolor adipisicing sint commodo magna aute excepteur. Deserunt laborum quis esse culpa magna consequat laboris pariatur adipisicing sint minim reprehenderit qui amet. Aute aute non ex eiusmod .",
   likesCount: 321,
   isLiked: true,
-  picturedAt: moment(new Date()).format("LLLL"),
+  picturedAt: moment(new Date()).format("LLL"),
 }));
 
 export const mockPosts3: SummaryPostListSchema = Array.from({ length: 1 }).map(() => ({
@@ -24,7 +24,7 @@ export const mockPosts3: SummaryPostListSchema = Array.from({ length: 1 }).map((
     "Dolore est ad nostrud id dolor. Sit ea consectetur labore et ea voluptate in do sit. Ut commodo deserunt laboris esse sint mollit amet labore officia irure eu cupidatat officia. Id non velit anim labore deserunt sint magna deserunt reprehenderit laboris ea ea consequat sit.",
   likesCount: 321,
   isLiked: true,
-  picturedAt: moment(new Date()).format("LLLL"),
+  picturedAt: moment(new Date()).format("LLL"),
 }));
 
 export const mockAllPost: SummaryPostListSchema = [...mockPosts, ...mockPosts2, ...mockPosts3];
