e613b4d327392ecc7552511e0535bc9646042425
nimod7890 <nimod1234@g.skku.edu>
2023-10-30 21:50:25 +0900

    [TSK-14] resize body width

--- src/app/components/base/MainContent.tsx
+++ src/app/components/base/MainContent.tsx
@@ -1,10 +1,14 @@
 import { Stack } from "@mui/material";
 import { Outlet } from "react-router-dom";
+import { Layout } from "../../constants/globalSizes";
 
 export default function MainContent() {
   return (
     <Stack sx={{ padding: "50px 10px 100px 10px", alignItems: "center" }}>
-      <Stack spacing={"10px"} sx={{ minWidth: "340px", maxWidth: "1000px", width: "100%" }}>
+      <Stack
+        spacing={"10px"}
+        sx={{ minWidth: Layout.bodyMinWidth, maxWidth: Layout.bodyMaxWidth, width: "100%" }}
+      >
         <Outlet />
       </Stack>
     </Stack>
