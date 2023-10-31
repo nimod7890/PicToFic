befb65578beedae9a11d5d1cfd5f37730429f88e
nimod7890 <nimod1234@g.skku.edu>
2023-10-31 20:21:03 +0900

    [TSK-13] get story by storyId in Story component

--- src/app/components/common/story/Story.tsx
+++ src/app/components/common/story/Story.tsx
@@ -1,5 +1,17 @@
+import { Box } from "@mui/material";
+import useGetStory from "../../../hooks/story/useGetStory";
+import StoryCard from "./card/StoryCard";
+
 type StoryContentProps = { storyId: number };
 
 export default function Story({ storyId }: StoryContentProps) {
-  return <>{storyId}</>;
+  const { story } = useGetStory({ storyId });
+
+  return (
+    <>
+      <Box width="400px">
+        <StoryCard story={story} />
+      </Box>
+    </>
+  );
 }
