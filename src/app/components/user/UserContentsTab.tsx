222992d1b386b2851ccb25c4541a4cf798dc157b
nimod7890 <nimod1234@g.skku.edu>
2023-11-02 00:00:09 +0900

    [TSK-16] create user contents navigation tab

---
+++ src/app/components/user/UserContentsTab.tsx
@@ -0,0 +1,51 @@
+import { Tabs, Paper, Tab } from "@mui/material";
+import { Link, Outlet } from "react-router-dom";
+import { getUserPagePath, getFramePagePath, getSnapPagePath } from "../../routes/routePath";
+import { useState } from "react";
+import {
+  AppsRounded as AppsRoundedIcon,
+  FavoriteRounded as FavoriteRoundedIcon,
+  FilterRounded as FilterRoundedIcon,
+} from "@mui/icons-material";
+
+type UserContentsTabProps = { userId: string };
+
+export default function UserContentsTab({ userId }: UserContentsTabProps) {
+  const [tabValue, setTabValue] = useState<number>(0);
+
+  return (
+    <>
+      <Tabs
+        variant="fullWidth"
+        value={tabValue}
+        component={Paper}
+        onChange={(_, value) => setTabValue(value)}
+      >
+        <Tab to={getUserPagePath(userId)} {...tabProps(0)} />
+        <Tab to={getFramePagePath(userId)} {...tabProps(1)} />
+        <Tab to={getSnapPagePath(userId)} {...tabProps(2)} />
+      </Tabs>
+      <Paper sx={{ padding: 1 }}>
+        <Outlet />
+      </Paper>
+    </>
+  );
+}
+
+function tabProps(index: number) {
+  return {
+    "icon": (() => {
+      switch (index) {
+        case 0:
+          return <AppsRoundedIcon />;
+        case 1:
+          return <FilterRoundedIcon />;
+        case 2:
+          return <FavoriteRoundedIcon />;
+      }
+    })(),
+    "id": `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+    "component": Link,
+  };
+}
